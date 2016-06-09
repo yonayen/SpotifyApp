3316728dc84bfce52b984ceac740519120c41cdf
Yonathan Ayenew <yonayen@gmail.com>
2016-06-08 18:18:12 -0700

    changes to functions

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // Global Variables
-var artistId = '0JDkhL4rjiPNEp92jAgJnS';
-var baseUrl = 'https://api.spotify.com';
+var baseUrl = 'https://api.spotify.com/v1/search?q=';
+var relatedUrl = 'https://api.spotify.com/v1/artists/';
 
 // --- Function takes query term, displays it and calls getRequest function with query -- //
 $(function() 
@@ -17,8 +17,17 @@ $(function()
 // -- Function sends request w/ query to API -- //
 function getRequest(searchTerm)
 {
-	$.getJSON(baseUrl + '/v1/artists/' + artistId + '/related-artists'  + searchTerm, function(data){
+	$.getJSON(baseUrl + searchTerm + '&type=artist', function(data){
 		console.log((data.artists));
+		showResults(data.artists);
+		getRelated(data.artists.items.genres.id);
+	});
+}
+
+function getRelated(firstResults)
+{
+	$.getJSON(relatedUrl + firstResults.artists.items.genres.id + '/related-artists', function(firstResults){
+		console.log((firstResults.artists));
 		showResults(data.artists);
 	});
 }
@@ -34,9 +43,9 @@ function showResults(results)
 		if (counter < 6) {
 			counter ++;
 			html += '<div class="row">' + '<div class="post">' +
-				'<a href="https://www.youtube.com/watch?v='+ arrayitem.id.videoId + ' "><img src="' + arrayitem.snippet.thumbnails.medium.url + '"/></a>' +
-				'<h3 class="title">' + arrayitem.snippet.title + '</h3>' +
-				'<p class="post_info">' + arrayitem.snippet.publishedAt + ' | ' + arrayitem.snippet.channelTitle + '</p>'+ 
+				'<a href="https://open.spotify.com/artist/'+ arrayitem.artists.items.genres.id + ' "><img src="' + arrayitem.artists.items.images.url + '"/></a>' +
+				'<h3 class="title">' + arrayitem.artists.items.name + '</h3>' +
+				'<p class="post_info">' + arrayitem.artists.items.genres + ' | ' + arrayitem.artists.items.popularity + '</p>'+ 
 				'</div>' +
           '</div>';
 		}
