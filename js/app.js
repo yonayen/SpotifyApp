038b882754c5270b19f5aced7399c42b8928f822
Yonathan Ayenew <yonayen@gmail.com>
2016-11-14 16:44:41 -0800

    modify baseUrl and relatedUrl

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Global Variables
-var baseUrl = 'https://api.spotify.com/v1/search?q=';
-var relatedUrl = 'https://api.spotify.com/v1/artists/';
-​
+// var baseUrl = 'https://api.spotify.com/v1/search?q=';
+// var relatedUrl = 'https://api.spotify.com/v1/artists/';
+// ​
 // --- Function takes query term, displays it and calls getRequest function with query -- //
 $(function()
 {
